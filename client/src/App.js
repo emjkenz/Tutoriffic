e7fec324e763f5a648e12bbd426d7681ca7f6272
MichealHamoud <micheal.hamoud11@outlook.com>
2023-07-25 20:14:24 +1000

    added dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Dashboard from "./pages/dashboard/dashboard";
 import NotFound from "./pages/NotFound";
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/homepage";
 const client = new ApolloClient({
   uri: "/graphql",
   cache: new InMemoryCache(),
--- client/src/pages/dashboard/dashboard.jsx
+++ client/src/pages/dashboard/dashboard.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
+import styles from './dashboard.module.css'
+import { Outlet, Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <section id='about'>
-      <h5>Welcome</h5>
-      <h2>--Student Name--</h2>
+    <section>
+      <h5 className={styles.classname}>Welcome</h5>
+      <h2 className={styles.classname}>--Student Name--</h2>
 
-          <article>
-            <h5>Attendance</h5>
+          <article className={styles.attendance}>
+            <h5 className={styles.attendance_text}>Attendance</h5>
+            <p>80/100%</p>
           </article>
 
-          <article>
-            <h5>Modules</h5>
+          <article className={styles.modules}>
+            <h5 className={styles.modules_text}>Modules</h5>
+            <ul>maths</ul>
+            <ul>english</ul>
+            <ul>science</ul>
           </article>
 
-          <article>
-            <h5>Assigments</h5>
+          <article className={styles.assigments}>
+            <h5 className={styles.assigments_text}>Assigments</h5>
+          <p>you have 3 assigments for maths due on the 2nd of august</p>
           </article>
 
     </section>
