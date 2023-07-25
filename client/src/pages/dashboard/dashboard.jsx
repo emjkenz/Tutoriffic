617abe25e5a3d7629fe1fa6263491044f361fb40
MichealHamoud <micheal.hamoud11@outlook.com>
2023-07-25 19:25:12 +1000

    added auth.js

---
+++ client/src/pages/dashboard/dashboard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <section id='about'>
+      <h5>Welcome</h5>
+      <h2>--Student Name--</h2>
+
+          <article>
+            <h5>Attendance</h5>
+          </article>
+
+          <article>
+            <h5>Modules</h5>
+          </article>
+
+          <article>
+            <h5>Assigments</h5>
+          </article>
+
+    </section>
+    )
+}
+
+export default Dashboard
