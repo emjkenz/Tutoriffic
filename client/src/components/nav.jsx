8ff7328b188266cc94ff5196ca1e573c3db2346f
MichealHamoud <micheal.hamoud11@outlook.com>
2023-07-25 21:15:51 +1000

    added some code in navbar

---
+++ client/src/components/nav.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import './navbar.css'
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Assignments">Assignments</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Attendance">Attendance</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Modules">Modules</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Dashboard">Dashboard</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Sign-up">Sign up</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
