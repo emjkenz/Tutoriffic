0db4010ea927e0eb2a1a8ec1b61a8f2903f2fbf7
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-27 21:28:24 +1000

    Added mantine

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { MantineProvider } from '@mantine/core';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
