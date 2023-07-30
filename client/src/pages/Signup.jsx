2669892360b62370e6db38ac364692bf115d533f
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-30 16:29:58 +1000

    Updated login and signin page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -3,8 +3,12 @@ import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
 import { ADD_USER } from "../utils/mutations";
-import { Center, Text, BackgroundImage } from "@mantine/core"
-import background from "../assets/plantRing.jpg"
+import { Center, Container, BackgroundImage, Grid, Title, Input, Button } from "@mantine/core"
+import background from "../Assets/teachers-desk.jpg"
+import styled from "styled-components"
+import { At, CursorText, Lock } from 'tabler-icons-react';
+import { StyledBreadcrumb } from "../components/StyledBreadcrumb";
+import { FormGroup } from "../components/FormGroup";
 
 function Signup(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
@@ -36,56 +40,72 @@ function Signup(props) {
     <BackgroundImage src={background}
       style={{ height: "90vh" }}
     >
-      <div className="container my-1">
-        <Link to="/login" style={{ color: "#fff" }}>← Go to Login</Link>
+      <StyledBreadcrumb color={"red"}>
+        <Link to="/login">← Go to Login</Link>
+      </StyledBreadcrumb>
 
-        <h1 style={{ color: "#fff" }}>Signup</h1>
-        <form onSubmit={handleFormSubmit}>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="firstName" style={{ color: "#fff" }}>First Name:</label>
-            <input
-              placeholder="First"
-              name="firstName"
-              type="firstName"
-              id="firstName"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="lastName" style={{ color: "#fff" }}>Last Name:</label>
-            <input
-              placeholder="Last"
-              name="lastName"
-              type="lastName"
-              id="lastName"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="email" style={{ color: "#fff" }}>Email:</label>
-            <input
-              placeholder="youremail@test.com"
-              name="email"
-              type="email"
-              id="email"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex-row space-between my-2">
-            <label htmlFor="pwd" style={{ color: "#fff" }}>Password:</label>
-            <input
-              placeholder="******"
-              name="password"
-              type="password"
-              id="pwd"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex-row flex-end">
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
+      <Container>
+        <Grid grow>
+          <Grid.Col span={12}>
+            <Center p="md">
+              <Title order={1} color="#fff">Signup</Title>
+            </Center>
+          </Grid.Col>
+          <Grid.Col span={12}>
+            <Center p="md">
+              <form onSubmit={handleFormSubmit}>
+                <FormGroup>
+                  <label htmlFor="firstName" style={{ color: "#fff" }}>First Name:</label>
+                  <Input
+                    placeholder="First"
+                    icon={<CursorText size={16} />}
+                    name="firstName"
+                    type="firstName"
+                    id="firstName"
+                    onChange={handleChange}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <label htmlFor="lastName" style={{ color: "#fff" }}>Last Name:</label>
+                  <Input
+                    placeholder="Last"
+                    icon={<CursorText size={16} />}
+                    name="lastName"
+                    type="lastName"
+                    id="lastName"
+                    onChange={handleChange}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <label htmlFor="email" style={{ color: "#fff" }}>Email:</label>
+                  <Input
+                    placeholder="youremail@test.com"
+                    icon={<At size={16} />}
+                    name="email"
+                    type="email"
+                    id="email"
+                    onChange={handleChange}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <label htmlFor="pwd" style={{ color: "#fff" }}>Password:</label>
+                  <Input
+                    placeholder="******"
+                    icon={<Lock size={16} />}
+                    name="password"
+                    type="password"
+                    id="pwd"
+                    onChange={handleChange}
+                  />
+                </FormGroup>
+                <div className="flex-row flex-end">
+                  <Button type="submit">Submit</Button>
+                </div>
+              </form>
+            </Center>
+          </Grid.Col>
+        </Grid>
+      </Container>
     </BackgroundImage>
   );
 }
