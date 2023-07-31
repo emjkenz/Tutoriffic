cf25ebd9df8a772f7161aa997a7d8ae1e8652cc8
MichealHamoud <micheal.hamoud11@outlook.com>
2023-07-31 19:37:11 +1000

    added some fixes to the loggin

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,14 +3,30 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type User {
     _id: ID
-    firstName: String
-    lastName: String
-    email: String
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
   }
 
-  type Auth {
-    token: ID
-    user: User
+  input UserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
+  type Student {
+    id: String!
+    firstName: String!
+    lastName: String!
+    dateOfBirth: String!
+    schoolingLevel: String!
   }
 
   enum Grade {
@@ -23,11 +39,16 @@ const typeDefs = gql`
   }
 
   type Listing {
-    grade: Grade!
+    grade: Grade
   }
 
   type Query {
-    GetGrade: Listing
+    GetGrade: [Listing]
+    students: [Student]
+  }
+
+  type Mutation {
+    addUser(userData: UserInput!): AuthPayload
   }
 `;
 
