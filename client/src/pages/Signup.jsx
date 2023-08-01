c0742cd9eb5ad3e09bb8c3ea1430b31f4b6f0b8f
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-08-01 21:19:24 +1000

    Refactored form submit

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -5,7 +5,6 @@ import Auth from "../utils/auth";
 import { ADD_USER } from "../utils/mutations";
 import { Center, Container, BackgroundImage, Grid, Title, Input, Button } from "@mantine/core"
 import background from "../Assets/teachers-desk.jpg"
-import styled from "styled-components"
 import { At, CursorText, Lock } from 'tabler-icons-react';
 import { StyledBreadcrumb } from "../components/StyledBreadcrumb";
 import { FormGroup } from "../components/FormGroup";
@@ -16,15 +15,10 @@ function Signup(props) {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const mutationResponse = await addUser({
-      variables: {
-        email: formState.email,
-        password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
-      },
+    const {data} = await addUser({
+      variables: {...formState}
     });
-    const token = mutationResponse.data.addUser.token;
+    const { token, user } = await data.addUser;
     Auth.login(token);
   };
 
