d56e6f0e4c211b87d83c79365317c2c741dfeed5
CGrace <c.grace5@yahoo.com.au>
2023-08-02 22:38:54 +1000

    assignment page mostly complete for now, starting work on linked pages

--- client/src/pages/Assignments.jsx
+++ client/src/pages/Assignments.jsx
@@ -1,5 +1,5 @@
 import { Center, Grid, Title, Text, Image, BackgroundImage } from "@mantine/core"
-import styles from './Assignments.css'
+import styles from './Assignments.module.css'
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import calculator from '../Assets/assignments/calculator2RS.jpg';
@@ -7,32 +7,35 @@ import book from '../Assets/assignments/openBookRS.jpg';
 import brushes from '../Assets/assignments/paintBrushesRS.jpg';
 import play from '../Assets/assignments/playButton2.jpg';
 import Logo from "../Assets/Logo.webp"
+import background from "../Assets/assignments/assignmentBackground.jpg"
 
 
 const Assignments = () => {
     return (
-        <div>
-            <Sidebar />
-
-            <img className="playButton" src={play} style={{ height: 600, width: 1000, position: 'relative', right: 90, }} />
-            <p className="assText" style={{ position: 'relative', right: 450, bottom: 400, }}>Click play to begin todays lesson!</p>
-        </>
+        <BackgroundImage src={background} style={{ height: "90vh" }}>
+            <div>
+                <Sidebar />
+                <img className="playButton" src={play} style={{ height: 600, width: 1000, position: 'relative', left: 900, top: 50, }} />
+                <h3 className="assText" style={{ position: 'relative', left: 1250, top: 40, }}>Click play to begin todays lesson!</h3>
+            </div>
+        </BackgroundImage >
     )
 }
-
 
 const Sidebar = () => {
     return (
         <div className={styles.Sidebar}>
             <div className={styles.Sidebar_title}>Menu</div>
             <ul className={styles.Sidebar_nav}>
-                <li>Maths</li>
-                <li>English</li>
-                <li>Science</li>
-                <li>I'm just trying to steal Tristans damn sidebar RAWR</li>
+                <button className={styles.mathButton}>Maths</button>
+                <button className={styles.englishButton}>English</button>
+                <button className={styles.scienceButton}>Sciences</button>
+                <button className={styles.artButton}>Arts</button>
+
             </ul>
         </div>
-    );
+
+    )
 }
 
 
