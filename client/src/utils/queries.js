fbc87dc2d886490034d665b988f7b00ec4fba785
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-08-03 02:16:16 +1000

    Updated client side queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -8,3 +8,29 @@ export const QUERY_LOGEDIN = gql`
     }
   }
 `;
+
+export const QUERY_ALL_QUIZZES = gql`
+  query quizzes {
+    quizzes {
+      id
+      title
+      description
+      date
+    }
+  }
+`;
+
+export const QUERY_QUIZ = gql`
+  query quiz($quizId: String!) {
+  quiz(id: $quizId) {
+    id
+    title
+    description
+    date
+    questions {
+      question
+      answers
+    }
+  }
+}
+`;
