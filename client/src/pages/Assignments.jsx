a617bc748f27ac26462be1720a2b06f50d7a00d7
CGrace <c.grace5@yahoo.com.au>
2023-08-03 17:48:20 +1000

    assignment page slightly less boring

--- client/src/pages/Assignments.jsx
+++ client/src/pages/Assignments.jsx
@@ -9,6 +9,7 @@ import play from '../Assets/assignments/playButton2.jpg';
 import Logo from "../Assets/Logo.webp"
 import background from "../Assets/assignments/assignmentBackground.jpg"
 import { StyledBreadcrumb } from "../components/StyledBreadcrumb";
+import { Container } from '@mantine/core';
 
 const Assignments = () => {
     return (
@@ -17,9 +18,12 @@ const Assignments = () => {
                 <Sidebar />
                 <Center sx={{ flexDirection: "column", marginLeft: '15%' }}>
                     <img className="playButton" src={play} style={{ height: 600, width: 1000 }} />
-                    <h3 className="assText">Click play to begin todays lesson!</h3>
+                    <h3 className="assText">Click play for this weeks rundown!</h3>
                 </Center>
             </div>
+            <Container size="30rem" px={2} style={{ position: 'relative', left: 250, top: 0, }}>
+                <h4>Class Leader: Mrs Paul</h4> Welcome back students for another week of learning, fun and discovery! Before you head off to your individual modules, please view the above video so you can gain an understanding of what we are learning about this week, how it builds upon last weeks lessons, and to get a sneak peak at some of the most fun activities we have planned for you!
+            </Container>
         </BackgroundImage >
     )
 }
